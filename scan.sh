946a511e8e82c21b6295a3f0a34a2dc8e1956d4c
Thiago Freitas <66418848+T466@users.noreply.github.com>
2023-12-26 16:03:32 -0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-scsin=$(ls -l /sys/class/scsi_host/ | wc -l)
-
-for((i = $scsin; i >= 1; i--))
-
-do
-
-echo "- - -" > /sys/class/scsi_host/host$i/scan
-
-    sleep 2
-
-done
+echo hello
